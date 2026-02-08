fc131d32953260f9415f70a22fc9cdf5a1e160cc
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-08 11:34:39 +0530

    Unit2_Syntax.php

---
+++ Unit2_Syntax.php
@@ -0,0 +1,39 @@
+<?php
+echo "Hello World!";
+
+// This is a comment
+# This is also a comment
+
+/*
+This is a
+multi-line comment
+*/
+
+$name = "will";
+$age = 20;
+
+$color = "red";
+$Color = "blue";  // Different variable
+
+echo "Welcome to PHP";
+print "Learning PHP";
+
+$number = 10;        // Integer
+$price = 99.99;     // Float
+$text = "PHP";      // String
+$status = true;     // Boolean
+
+$b = 5;
+echo $a + $b;   // Arithmetic operator
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <?php
+// echo "Hello from PHP!";
+// ?>
+
+// </body>
+// </html>
+?>
