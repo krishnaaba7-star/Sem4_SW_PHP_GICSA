82cea7b40166b0bbd329d5eed4c640cfbf79e5fd
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-27 10:38:15 +0530

    Unit2_Variable_Scope.php

---
+++ Unit2_Variable_Scope.php
@@ -0,0 +1,50 @@
+<?php
+// function test() {
+//     $x = 10;   // Local variable
+//     echo $x;
+// }
+
+// test();  // Output: 10
+
+// $x = 20;  // Global variable
+
+// function test() {
+//     echo $x;  // Error (Cannot access directly)
+// }
+
+// test();
+
+// $x = 20;
+
+// function test() {
+//     global $x;
+//     echo $x;
+// }
+
+// test();  // Output: 20
+
+// $x = 30;
+
+// function test() {
+//     echo $GLOBALS['x'];
+// }
+
+// test();  // Output: 30
+
+// function counter() {
+//     static $count = 0;
+//     $count++;
+//     echo $count . "<br>";
+// }
+
+// counter();  // 1
+// counter();  // 2
+// counter();  // 3
+
+
+// function add($a, $b) {
+//     return $a + $b;
+// }
+
+// echo add(5, 3);  // 8
+?>
